913ee25c78e65ff8684ed4e8998f7675c8a67aef
CharlyG <carlo.gammarota@gmail.com>
2024-05-04 04:45:04 -0300

    'update'

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,24 +1,28 @@
 // products-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
-  const modelName = 'products';
-  const mongooseClient = app.get('mongooseClient');
+  const modelName = "products";
+  const mongooseClient = app.get("mongooseClient");
   const { Schema } = mongooseClient;
-  const schema = new Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    price: { type: Number, required: true },
-    images: { type: Array, required: true },
-    metaData: { type: Object, required: true },
-    category: { type: Object, required: true },
-    user_id: { type: String, required: true },
-    user: { type: Object, required: true },
-    slug: { type: String, required: true },
-  }, {
-    timestamps: true
-  });
+  const schema = new Schema(
+    {
+      title: { type: String, required: true },
+      content: { type: String, required: true },
+      price: { type: Number, required: true },
+      images: { type: Array, required: true },
+      metaData: { type: Object, required: true },
+      category: { type: Object, required: true },
+      user_id: { type: String, required: true },
+      user: { type: Object, required: true },
+      slug: { type: String, required: true },
+      quantity: { type: Number, required: true },
+    },
+    {
+      timestamps: true,
+    }
+  );
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
@@ -26,5 +30,4 @@ module.exports = function (app) {
     mongooseClient.deleteModel(modelName);
   }
   return mongooseClient.model(modelName, schema);
-  
 };
