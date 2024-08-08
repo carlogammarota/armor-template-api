ad07dbbaa6fc0a841169a99edf2b1b9ff3c77fd2
Server Local Ubuntu <carlo.gammarota@gmail.com>
2024-08-08 15:22:10 -0300

    update

--- src/hooks/port-count.js
+++ src/hooks/port-count.js
@@ -3,6 +3,17 @@ module.exports = (options = {}) => {
     const { app, service, data } = context;
 
     
+    //si el subdominio ya esta en uso no se puede crear
+    const subdomain = data.subdomain;
 
+    const subdomainExists = await service.find({
+      query: {
+        subdomain
+      }
+    });
+
+    if(subdomainExists.total > 0){
+      throw new Error('Subdomain already exists');
+    }
   };
 };
