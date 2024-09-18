4aff5b4e93b54156f2baf3055100d5d6882a3856
Carlo Gammarota <carlo.gammarota@gmail.com>
2024-09-18 05:25:22 -0300

    update

--- src/hooks/create-app.js
+++ src/hooks/create-app.js
@@ -3,133 +3,122 @@
 const axios = require("axios");
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
-  return async context => {
+  return async (context) => {
     // console.log("create-app", context.data)
-        
-      // return {
 
-      //hacer logica solo dejar crear si el usuario tiene el plan Premium 
-          
-      const data = context.data;
-      context.data.link = "https://"+data.subdomain+".armortemplate.site";
-      context.data.status = "creating";
-      context.data.restaurant = {
-        "active": true,
-        "forceHome": true,
-        "title": "Menu Restaurant by Armor Template",
-        "instagram": "",
-        "location": "",
-        "telephone": 0,
-        "email": ""
-    }
+    // return {
 
+    //hacer logica solo dejar crear si el usuario tiene el plan Premium
 
+    const data = context.data;
+    context.data.link = "https://" + data.subdomain + ".armortemplate.site";
+    context.data.status = "creating";
+    context.data.restaurant = {
+      active: true,
+      forceHome: true,
+      title: "Menu Restaurant by Armor Template",
+      instagram: "",
+      location: "",
+      telephone: 0,
+      email: "",
+    };
 
+    context.data.plugins = {
+      mercadopago: {
+        mercadopago_token: "",
+      },
+    };
 
     let sett = await axios.get("https://api.armortemplate.site/settings/", {
-        query: {
-          $limit: 1,
-        }
+      query: {
+        $limit: 1,
+      },
     });
 
-    let setting = sett.data
+    let setting = sett.data;
     setting = setting.data[0];
 
     context.data.version = setting.version;
 
-      // const frontend_port = 2002
-      // const api_port = 1002
+    // const frontend_port = 2002
+    // const api_port = 1002
+
+    try {
+      const port = await context.app.service("ports").find({
+        query: {
+          $limit: 100,
+          // $sort: {
+          //   api_port: 1,
+          // },
+        },
+      });
+      console.log("port", port.total);
+      // if (port.total == 0) {
+      //   console.log("No hay puertos");
+      //   return {
+      //     message: "No hay puertos disponibles",
+      //   };
+      // }
+      const port_api = port.total + 1002;
+      const port_frontend = port.total + 2002;
+
+      console.log("port_api", port_api);
+      console.log("port_frontend", port_frontend);
 
       try {
-        const port = await context.app.service("ports").find({
-          query: {
-            $limit: 100,
-            // $sort: {
-            //   api_port: 1,
-            // },
-          },
+        const setPorts = await context.app.service("ports").create({
+          api_port: port_api,
+          frontend_port: port_frontend,
+          subdomain: data.subdomain,
         });
-        console.log("port", port.total);
-        // if (port.total == 0) {
-        //   console.log("No hay puertos");
-        //   return {
-        //     message: "No hay puertos disponibles",
-        //   };
-        // }
-        const port_api = port.total + 1002;
-        const port_frontend = port.total + 2002;
-
-        console.log("port_api", port_api);
-        console.log("port_frontend", port_frontend);
-
-
-        try {
-          const setPorts = await context.app.service("ports").create({
-            api_port: port_api,
-            frontend_port: port_frontend,
-            subdomain: data.subdomain,
-          });
-          console.log("setPorts", setPorts);
-          context.data.ports_id = setPorts._id;
-          context.data.ports = setPorts;  
-        
-        } catch (error) {
-          console.log("error", error);
-          //devolver error de feathers
-          throw new Error("No se pudo asignar los puertos");
-          
-        }
-
-        return context
-        
+        console.log("setPorts", setPorts);
+        context.data.ports_id = setPorts._id;
+        context.data.ports = setPorts;
       } catch (error) {
         console.log("error", error);
-        return {
-          message: "Error al crear la aplicación",
-        }
-        // throw error; // Lanza una excepción si hay un error.
+        //devolver error de feathers
+        throw new Error("No se pudo asignar los puertos");
       }
 
+      return context;
+    } catch (error) {
+      console.log("error", error);
+      return {
+        message: "Error al crear la aplicación",
+      };
+      // throw error; // Lanza una excepción si hay un error.
+    }
 
-      return "dev"
-  
-      console.log("Create App");
-      
+    return "dev";
 
+    console.log("Create App");
 
+    // try {
+    //   const response = await axios.post(
+    //     "http://64.227.76.217:3131/create-app",
+    //     //IMPORTANTE: faltan mandar contraseña
+    //     {
+    //       "subdomain": "ibot-design",
+    //       "api_port": 1001,
+    //       "frontend_port": 2001
+    //     }
+    //   );
+    // } catch (error) {
+    //   console.log("error", error);
+    //   return {
+    //     message: "Error al crear la aplicación",
+    //   }
+    //   // throw error; // Lanza una excepción si hay un error.
+    // }
 
-
-      // try {
-      //   const response = await axios.post(
-      //     "http://64.227.76.217:3131/create-app",
-      //     //IMPORTANTE: faltan mandar contraseña 
-      //     {
-      //       "subdomain": "ibot-design",
-      //       "api_port": 1001,
-      //       "frontend_port": 2001
-      //     }           
-      //   );
-      // } catch (error) {
-      //   console.log("error", error);
-      //   return {
-      //     message: "Error al crear la aplicación",
-      //   }
-      //   // throw error; // Lanza una excepción si hay un error.
-      // }
-      
-
-      // return context
-      // // await 
-      // } catch (error) {
-      //   console.log("error", error);
-      //   return {
-      //     message: "Error al crear la aplicación",
-      //   }
-      //   // throw error; // Lanza una excepción si hay un error.
-      // }
-      
-      
-
-    
+    // return context
+    // // await
+    // } catch (error) {
+    //   console.log("error", error);
+    //   return {
+    //     message: "Error al crear la aplicación",
+    //   }
+    //   // throw error; // Lanza una excepción si hay un error.
+    // }
   };
 };
